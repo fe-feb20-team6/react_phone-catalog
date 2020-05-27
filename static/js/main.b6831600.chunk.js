4373c01adb94e99aec1c5b2e40c0ab456ed0b99a
kochatkov <vlad.kochatkov@gmail.com>
2020-05-27 14:33:39 +0300

    Updates

--- static/js/main.b6831600.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact_phone-catalog"]=this["webpackJsonpreact_phone-catalog"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a),o=n(2),r=n.n(o),l=(n(8),function(){return c.a.createElement("div",{className:"App"},c.a.createElement("h1",null,"React Phone Catalog1111"))});r.a.render(c.a.createElement(l,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.b6831600.chunk.js.map
